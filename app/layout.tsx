fe685d392c3056a9585f6ccf36e2b2e3f20ba299
Shahina-Sk <samanshaik2020@gmail.com>
2026-02-04 17:34:21 +0530

    feat: integrate Google AdSense and Google Tag Manager
    - Added Google AdSense script to RootLayout for monetization.
    - Implemented Google Tag Manager (GTM) with the required script in <head> and noscript fallback in <body>.

--- app/layout.tsx
+++ app/layout.tsx
@@ -162,6 +162,15 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WR6DMT62');`}
+        </Script>
+
         {/* Google Analytics (gtag.js) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-EYFHG074VZ"
@@ -175,6 +184,14 @@ export default function RootLayout({
             gtag('config', 'G-EYFHG074VZ');
           `}
         </Script>
+
+        {/* Google AdSense */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9833112183659445"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
 
         {/* Preconnect for performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -210,6 +227,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WR6DMT62"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Providers>{children}</Providers>
       </body>
     </html>
